5f84ac4dde93478bed145a8cfca2b085e2674ab4
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-16 19:25:28 +0100

    Import Bootstrap-Flask to make styling easier

--- src/server.py
+++ src/server.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap5
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+bootstrap = Bootstrap5(app)
 
 
 @app.get('/')
