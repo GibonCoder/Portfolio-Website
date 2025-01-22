8f6e68b38f3ad277e22690f7d17a2317a8350ef0
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-22 21:53:13 +0100

    Add route for contact-me subpage

--- src/server.py
+++ src/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap5
+from forms import ContactForm
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 bootstrap = Bootstrap5(app)
@@ -9,9 +10,16 @@ bootstrap = Bootstrap5(app)
 def index():
     return render_template('index.html')
 
+
 @app.get('/about-me')
 def about_me():
     return render_template('about-me.html')
+
+
+@app.get('/contact-me')
+def contact_me():
+    contact_form = ContactForm()
+    return render_template('contact-me.html', form=contact_form)
 
 
 if __name__ == '__main__':
