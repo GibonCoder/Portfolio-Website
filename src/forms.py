88eeb4d56aaba808edbd70c7e77d903cfd05b745
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-24 23:19:10 +0100

    Modify labels (add asterix for required fields). Add message to Email validator

--- src/forms.py
+++ src/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, StringField, EmailField, SubmitField
+from wtforms import TextAreaField, StringField, EmailField, SubmitField, Label
 from wtforms.validators import DataRequired, Email, length
 
 
 class ContactForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
-    email = EmailField("Email", validators=[DataRequired(), Email()])
-    content = TextAreaField("Content", validators=[DataRequired(), length(max=400)])
+    email = EmailField("*Email", validators=[DataRequired(), Email("Please enter a valid email address")])
+    content = TextAreaField("*Message", validators=[DataRequired(), length(max=400)])
     submit = SubmitField("Send Message")
