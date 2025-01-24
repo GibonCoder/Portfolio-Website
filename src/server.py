f7c0b63ec4ac2e663a075afc94cc4cb1833ae7d4
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-24 23:23:00 +0100

    Add functionality to check if form is validated. Add flash messages to template.

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
 from flask_bootstrap import Bootstrap5
 from forms import ContactForm
 
@@ -11,19 +11,23 @@ app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
 bootstrap = Bootstrap5(app)
 
 
-@app.get('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.get('/about-me')
+@app.route('/about-me')
 def about_me():
     return render_template('about-me.html')
 
 
-@app.get('/contact-me')
+@app.route('/contact-me', methods=['GET', 'POST'])
 def contact_me():
     contact_form = ContactForm()
+    if contact_form.validate_on_submit():
+        flash('Message sent successfully')
+        # TODO: Add functionality for sending emails
+        return render_template('contact-me.html', form=contact_form)
     return render_template('contact-me.html', form=contact_form)
 
 
