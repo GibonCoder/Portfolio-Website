65e2674cbc324d612dfcd3d03c5517ac53c3e281
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-25 18:14:26 +0100

    Add default value for the title field

--- src/forms.py
+++ src/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Email, length
 
 
 class ContactForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
+    title = StringField("Title", default="Contact request")
     email = EmailField("*Email", validators=[DataRequired(), Email("Please enter a valid email address")])
     content = TextAreaField("*Message", validators=[DataRequired(), length(max=400)])
     submit = SubmitField("Send Message")
