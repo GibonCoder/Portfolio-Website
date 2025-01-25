08c0d3786b94052cb5a05cd469c393c23cf9c7d5
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-25 19:22:49 +0100

    Create route for adding new project to the website (dummy authentication for now). Add displaying flash messages to the index in case of unathorised attempt of entering add-project route. Create simple page for add-project route with form

--- src/server.py
+++ src/server.py
@@ -1,15 +1,17 @@
 import os
 from datetime import date
 from dotenv import load_dotenv
-from flask import Flask, render_template, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
-from forms import ContactForm
+from flask_ckeditor import CKEditor
+from forms import ContactForm, AddProjectForm
 
 load_dotenv()
 
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY')
 bootstrap = Bootstrap5(app)
+ckeditor = CKEditor(app)
 
 
 @app.context_processor
@@ -35,6 +37,22 @@ def contact_me():
         # TODO: Add functionality for sending emails
         return render_template('contact-me.html', form=contact_form)
     return render_template('contact-me.html', form=contact_form)
+
+
+@app.route('/add-project/<login>/<password>', methods=['GET', 'POST'])
+def add_project(login, password):
+    if login == 'admin' and password == 'admin':  # dummy values. TODO: Change for actual values
+        project_form = AddProjectForm()
+        if project_form.validate_on_submit():
+            flash('Project added successfully')
+            return redirect(url_for('index'))   # TODO: Change for projects page
+        else:
+            return render_template('add-project.html', form=project_form)
+    else:
+        flash("You don't have permission to add new projects")
+        return redirect(url_for('index'))
+
+
 
 
 if __name__ == '__main__':
