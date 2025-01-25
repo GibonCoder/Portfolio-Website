047bb5ddd1c09311d0f4d8c315ad833eb712e662
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-01-25 19:16:17 +0100

    Create form for adding projects to the website

--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, StringField, EmailField, SubmitField, Label
 from wtforms.validators import DataRequired, Email, length
+from flask_ckeditor import CKEditorField
 
 
 class ContactForm(FlaskForm):
@@ -8,3 +9,10 @@ class ContactForm(FlaskForm):
     email = EmailField("*Email", validators=[DataRequired(), Email("Please enter a valid email address")])
     content = TextAreaField("*Message", validators=[DataRequired(), length(max=400)])
     submit = SubmitField("Send Message")
+
+
+class AddProjectForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    link = StringField("GitHub Link", validators=[DataRequired()])
+    description = CKEditorField("Description", validators=[DataRequired()])
+    submit = SubmitField("Add Project")
