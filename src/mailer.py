25da990d722647cafb4d9ac7c0548ae61682301e
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-06 20:16:41 +0100

    Add sender argumnt inside Message class instance in both functions

--- src/mailer.py
+++ src/mailer.py
@@ -8,15 +8,17 @@ class Mailer:
     def send_contact_mail(self, cm_subj, rp_email, cm_message, cm_email):
         msg = Message(
             subject=cm_subj,
+            sender=cm_email,
             recipients=[cm_email],
             body=f'''You have received a new message from {rp_email}:
             {cm_message}'''
         )
         self._mail.send(msg)
 
-    def send_confirmation_mail(self, rp_email):
+    def send_confirmation_mail(self, cm_mail, rp_email):
         msg = Message(
             subject="Thanks for contacting me!",
+            sender=cm_mail,
             recipients=[rp_email],
             body='''Hey there!\n Thanks for reaching out to me. I will get back to you as soon as possible.'''
         )
