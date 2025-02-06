ca654dfec345f926f71cb5ee6b3791f8f2a0037b
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-06 19:51:34 +0100

    Create simple mailing system for sending message

---
+++ src/mailer.py
@@ -0,0 +1,23 @@
+from flask_mail import Mail, Message
+
+
+class Mailer:
+    def __init__(self, flask_app):
+        self._mail = Mail(flask_app)
+
+    def send_contact_mail(self, cm_subj, rp_email, cm_message, c_email):
+        msg = Message(
+            subject=cm_subj,
+            recipients=[c_email],
+            body=f'''You have received a new message from {rp_email}:
+            {cm_message}'''
+        )
+        self._mail.send(msg)
+
+    def send_confirmation_mail(self, rp_email):
+        msg = Message(
+            subject="Thanks for contacting me!",
+            recipients=[rp_email],
+            body='''Hey there!\n Thanks for reaching out to me. I will get back to you as soon as possible.'''
+        )
+        self._mail.send(msg)
