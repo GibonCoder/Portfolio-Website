50eff82e6f6fa9e53293e6d1d2bbed5ed46c546d
Szymon Tomasiak <sz.tomasiak@gmail.com>
2025-02-06 20:04:30 +0100

    slight change of the parameter

--- src/mailer.py
+++ src/mailer.py
@@ -5,10 +5,10 @@ class Mailer:
     def __init__(self, flask_app):
         self._mail = Mail(flask_app)
 
-    def send_contact_mail(self, cm_subj, rp_email, cm_message, c_email):
+    def send_contact_mail(self, cm_subj, rp_email, cm_message, cm_email):
         msg = Message(
             subject=cm_subj,
-            recipients=[c_email],
+            recipients=[cm_email],
             body=f'''You have received a new message from {rp_email}:
             {cm_message}'''
         )
